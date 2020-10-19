b58fa18b3d63fd6937c3036c9b4498cc1c7a9404
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 16:07:42 +0000

    Working debugs for ssh

--- my_fixtures/host_fixture.py
+++ my_fixtures/host_fixture.py
@@ -5,21 +5,21 @@ from testtools.content import text_content,attach_file
 
 class HostFixture(fixtures.Fixture):
     def _setUp(self):
-        self.addDetail('arbitrary-color-name', text_content("blue"))
-
         self.hostUser = os.getenv("HOST_USER")
         self.hostKey = os.getenv("HOST_PK")
         self.host = os.getenv("HOST_URL")
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         flag = False
+        connection = ""
         try:
-            connaction = client.connect(self.host, username=self.hostUser, pkey=self.hostKey, timeout=5)
-        except Exception:
+            connection = client.connect(self.host, username=self.hostUser, pkey=self.hostKey, timeout=5)
+        except Exception as e:
+            self.addDetail('error', text_content(str(e.msg)))
             flag = True
-#        client.close()
+        client.close()
         self.addCleanup(delattr, self, 'hostUser')
         self.addCleanup(delattr, self, 'hostKey')
         self.addCleanup(delattr, self, 'host')
-        self.addDetail('arbitrary-color-name2', text_content(str(connaction)))
+    
 
