0f7bb4877f42f4e123f53548db23ee1b871eced4
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-10-21 15:15:45 +0000

    Small fixes

--- scripts/test_bash_jumphost.py
+++ scripts/test_bash_jumphost.py
@@ -7,11 +7,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class BashJumphostTests(WithAttributes,BaseTestCase):
-
-    def test_square(self):
-        logger = logging.getLogger(__name__ + '.test_square')
-        self.check_cmd_on_host("pwd",logger)
-    
     def test_1(self):
         logger = logging.getLogger(__name__ + '.test1')
         self.run_bash_test_on_host('test1.sh',logger)
