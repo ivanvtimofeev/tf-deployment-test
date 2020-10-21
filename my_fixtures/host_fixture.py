6e0da7b59d10e81437672899e374eb4ec97d7f7b
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-10-21 10:51:29 +0000

    HostFixture looks works

--- my_fixtures/host_fixture.py
+++ my_fixtures/host_fixture.py
@@ -13,18 +13,22 @@ class HostFixture(fixtures.Fixture):
         self.hostUser = os.getenv("HOST_USER")
         self.hostKey = os.getenv("HOST_PK")
         self.host = os.getenv("HOST_URL")
+        with open(os.path.expanduser('pk.key'),'w') as fd:
+            fd.write( self.hostKey )
 
         if( not self.hostUser or not self.hostKey or not self.host):
             raise AssertionError("ERROR: Need to pass host credentials to  run the tests")
 
+
+        key = paramiko.RSAKey.from_private_key_file('pk.key')
         self._client = paramiko.SSHClient()
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.connection = self._client.connect(self.host, username=self.hostUser, pkey=paramiko.RSAKey(data = bytes(self.hostKey,'utf-8')), timeout=5)
+        self.connection = self._client.connect(self.host, username=self.hostUser, key_filename = os.path.expanduser('pk.key'), timeout=5)
 
         self.addCleanup(delattr, self, 'hostUser')
         self.addCleanup(delattr, self, 'hostKey')
         self.addCleanup(delattr, self, 'host')
-        self.addCleanup(_closeSSHConnection,self)
+        self.addCleanup(self._closeSSHConnection)
     
     def _closeSSHConnection(self):
         self._client.close()
