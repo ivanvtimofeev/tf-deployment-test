ce7dbaceba5efc776c89e5227702b7f689b42703
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-10-22 10:26:53 +0000

    Add test test_manifests_k8s_smoke

--- scripts/test_bash_jumphost.py
+++ scripts/test_bash_jumphost.py
@@ -13,3 +13,7 @@ class BashJumphostTests(WithAttributes,BaseTestCase):
         logger = logging.getLogger(__name__ + '.test2')
         self.run_bash_test_on_host('test2.sh',logger)
 
+    @attr(["orchestrator_k8s","deployer_manifests"])
+    def test_manifests_k8s_smoke(self):
+        logger = logging.getLogger(__name__ + '.manifests_k8s_smoke')
+        self.run_bash_test_on_host('k8s_manifests_k8s.sh',logger)
